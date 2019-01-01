b8babb77f3ca11fdaa76bdd0926040cf42fb0930
frdvorak <frantisek.dvorak011@gmail.com>
2019-01-01 20:54:52 +0000

    add new food item through form

--- app.js
+++ app.js
@@ -22,6 +22,22 @@ const ItemCtrl = (function(){
         getItems: function(){
             return data.items;
         },
+        addItem: function(name, calories){
+            // Create ID
+            let ID;
+            if(data.items.length > 0){
+                ID = data.items[data.items.length-1].id + 1;
+            } else {
+                ID = 0;
+            }
+            // Callories to number
+            calories = parseInt(calories);
+            // Create new item
+            newItem = new Item(ID, name, calories);
+            // Add to items array
+            data.items.push(newItem);
+            return newItem;
+        },
         logData: function(){
             return data;
         }
@@ -32,9 +48,13 @@ const ItemCtrl = (function(){
 const UICtrl = (function(){
     const UISelectors = {
         itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories'
     }
     // Public methods
     return {
+        // In App we fetched our items from the ItemCtrl(item controller) and then used UICtrl to insert them to the interface
         populateItemList: function(items){
             let html = '';
             items.forEach(function(item){
@@ -49,12 +69,41 @@ const UICtrl = (function(){
             });
             // Insert list items
             document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getItemInput: function(){
+            return {
+                name: document.querySelector(UISelectors.itemNameInput).value,
+                calories: document.querySelector(UISelectors.itemCaloriesInput).value
+            }
+        },
+        // make UISelectors public so we can use it in loadEventListeners later
+        getSelectors: function(){
+            return UISelectors;
         }
     }
 })();
 
 // App Controller
 const App = (function(ItemCtrl, UICtrl){
+    // Load event listeners
+    const loadEventListeners = function(){
+        // Get UISelectors
+        const UISelectors = UICtrl.getSelectors();
+        // Add item event
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+    }
+    // Add item submit
+    const itemAddSubmit = function(e){
+        // Get form input from UI Controller
+        const input = UICtrl.getItemInput();
+        // Check for name and calorie input
+        if(input.name !== '' && input.calories !== ''){
+            // Add item
+            const newItem = ItemCtrl.addItem(input.name, input.calories);
+        }
+        console.log(input);
+        e.preventDefault();
+    }
     // Public methods
     return {
         // Init function, anything we need right away as the app loads
@@ -62,7 +111,9 @@ const App = (function(ItemCtrl, UICtrl){
             // Fetch items from data structure
             const items = ItemCtrl.getItems();
             // Populate list with items
-            UICtrl.populateItemList(items); 
+            UICtrl.populateItemList(items);
+            // Load event listeners
+            loadEventListeners();
         }
     }
 })(ItemCtrl, UICtrl);
