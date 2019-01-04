d9041acc10ac2e8e97396a12595c2233b5dfd751
frdvorak <frantisek.dvorak011@gmail.com>
2019-01-04 08:14:21 +0000

    enter edit state + update button click event

--- app.js
+++ app.js
@@ -38,6 +38,22 @@ const ItemCtrl = (function(){
             data.items.push(newItem);
             return newItem;
         },
+        getItemById: function(id){
+            let found = null;
+            // Loop through items
+            data.items.forEach(function(item){
+                if(item.id === id){
+                    found = item;
+                }
+            });
+            return found;
+        },
+        setCurrentItem: function(item){
+            data.currentItem = item;
+        },
+        getCurrentItem: function(){
+            return data.currentItem;    
+        },
         getTotalCalories: function(){
             let total = 0;
             data.items.forEach(function(item){
@@ -114,6 +130,11 @@ const UICtrl = (function(){
             document.querySelector(UISelectors.itemNameInput).value = '';
             document.querySelector(UISelectors.itemCaloriesInput).value = '';
         },
+        addItemToForm: function(){
+            document.querySelector(UISelectors.itemNameInput).value = ItemCtrl.getCurrentItem().name;
+            document.querySelector(UISelectors.itemCaloriesInput).value = ItemCtrl.getCurrentItem().calories;
+            UICtrl.showEditState();
+        },
         // Hide UL element when needed
         hideList: function(){
             document.querySelector(UISelectors.itemList).style.display= 'none';
@@ -127,6 +148,12 @@ const UICtrl = (function(){
             document.querySelector(UISelectors.deleteBtn).style.display= 'none';
             document.querySelector(UISelectors.backBtn).style.display= 'none';
             document.querySelector(UISelectors.addBtn).style.display= 'inline';
+        },
+        showEditState: function(){
+            document.querySelector(UISelectors.updateBtn).style.display= 'inline';
+            document.querySelector(UISelectors.deleteBtn).style.display= 'inline';
+            document.querySelector(UISelectors.backBtn).style.display= 'inline';
+            document.querySelector(UISelectors.addBtn).style.display= 'none';
         },
         // Make UISelectors public so we can use it in loadEventListeners later
         getSelectors: function(){
@@ -143,9 +170,17 @@ const App = (function(ItemCtrl, UICtrl){
         const UISelectors = UICtrl.getSelectors();
         // Add item event
         document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+        // Disable submit on enter
+        document.addEventListener('keypress', function(e){
+            if(e.keycode === 13 || e.which === 13){
+                e.preventDefault();
+                return false;
+            }
+        });
         // Edit icon click event
-        document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
-
+        document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
+        // Update item event
+        document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
     }
     // Add item submit
     const itemAddSubmit = function(e){
@@ -168,19 +203,29 @@ const App = (function(ItemCtrl, UICtrl){
         e.preventDefault();
     }
     // Update item submit
-    const itemUpdateSubmit = function(e){
+    const itemEditClick = function(e){
         if(e.target.classList.contains('edit-item')){
             // Get list item id
             const listId = e.target.parentNode.parentNode.id;
             // Break into an array
             const listIdArr = listId.split('-');
             // Get the actual id
-            const id = parseInd(listIdArr[1]);
+            const id = parseInt(listIdArr[1]);
             // Get item
             const itemToEdit = ItemCtrl.getItemById(id);
+            // Set current item
+            ItemCtrl.setCurrentItem(itemToEdit);
+            // Add item to form
+            UICtrl.addItemToForm();
         }
         e.preventDefault();
     }
+    // Update item submit
+    const itemUpdateSubmit = function(e){
+        console.log('update');
+        e.preventDefault();
+    }
+
     // Public methods
     return {
         // Init function, anything we need right away as the app loads
