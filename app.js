5bfcdce17ef021660781d40a6c1ad4937d0f437d
frdvorak <frantisek.dvorak011@gmail.com>
2019-01-05 16:51:55 +0000

    total calories count updating correctly

--- app.js
+++ app.js
@@ -258,6 +258,11 @@ const App = (function(ItemCtrl, UICtrl){
         const updatedItem = ItemCtrl.updateItem(input.name, input.calories);
         // Update UI
         UICtrl.updateListItem(updatedItem);
+        // Get total calories
+        const totalCalories = ItemCtrl.getTotalCalories();
+        // Add total calories to UI
+        UICtrl.showTotalCalories(totalCalories);
+        UICtrl.clearEditState();
         e.preventDefault();
     }
 
